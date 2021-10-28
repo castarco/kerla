8b56f4f7efda2dff5c07956a94e4757b57c48135
Andres Correa Casablanca <castarco@coderspirit.xyz>
2021-10-28 17:39:26 +0200

    refactor: use enum instead of "free" constants

--- kernel/syscalls/lseek.rs
+++ kernel/syscalls/lseek.rs
@@ -2,11 +2,13 @@ use crate::ctypes::{c_int, c_off};
 use crate::{fs::opened_file::Fd, result::Result};
 use crate::{process::current_process, syscalls::SyscallHandler};
 
-const SEEK_SET: c_int = 0;
-const SEEK_CUR: c_int = 0;
-const SEEK_END: c_int = 0;
-const SEEK_DATA: c_int = 0;
-const SEEK_HOLE: c_int = 0;
+pub enum WhenceValues {
+    SEEK_SET = 0,  // seek relative to beginning of file
+    SEEK_CUR = 1,  // seek relative to current file position
+    SEEK_END = 2,  // seek relative to end of file
+    SEEK_DATA = 3, // seek to the next data
+    SEEK_HOLE = 4, // seek to the next hole
+}
 
 impl<'a> SyscallHandler<'a> {
     pub fn sys_lseek(&mut self, fd: Fd, offset: c_off, whence: c_int) -> Result<isize> {
